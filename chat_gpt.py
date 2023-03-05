79f2270b6b8924643e17e480589052ee4562c2b1
Jalil <benharkatdjalil@gmail.com>
2023-03-05 16:51:18 +0400

    first commit

--- chat_gpt.py
+++ chat_gpt.py
@@ -1,12 +1,14 @@
 from revChatGPT.V1 import Chatbot, AsyncChatbot
 from decouple import config
 
-chatbot = AsyncChatbot(
-    config={"email": config("chatGPT_email"), "password": config("chatGPT_password")}
-)
-
 
 async def query_chatGPT(prompt_):
+    chatbot = AsyncChatbot(
+        config={
+            "email": config("chatGPT_email"),
+            "password": config("chatGPT_password"),
+        }
+    )
     prompt = f"""{prompt_}, in json format following this format ```["domain1.com", "domain2.com", "domain3.com"...etc]```"""
     response = ""
 
